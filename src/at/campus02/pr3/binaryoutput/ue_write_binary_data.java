26044cce4312a6333cf032b040aecfefd80314a0
Your Name <you@example.com>
2020-06-08 15:26:52 +0200

    update

---
+++ src/at/campus02/pr3/binaryoutput/ue_write_binary_data.java
@@ -0,0 +1,32 @@
+package at.campus02.pr3.binaryoutput;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ue_write_binary_data {
+    /*
+    Java FileOutputStream is an output stream used for writing data to a file.
+
+    If you have to write primitive values into a file,
+     use FileOutputStream class. You can write byte-oriented as
+     well as character-oriented data through FileOutputStream class.
+      But, for character-oriented data, it is preferred to use FileWriter than FileOutputStream.
+     */
+    public static void main(String[] args) throws IOException {
+        File file = new File("test-output.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+        char c;
+
+        while ((c = (char) System.in.read()) != 'x') {
+            fileOutputStream.write(c);
+            System.out.println(c);
+        }
+        fileOutputStream.flush();
+        fileOutputStream.close();
+        System.out.println("Closed");
+    }
+
+}
