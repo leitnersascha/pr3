cf3996e443e0bd5d9501efe08b6bf1a9be9097ac
Your Name <you@example.com>
2020-06-21 07:21:19 +0200

    update

---
+++ src/at/campus02/pr3/file/ue_rename_and_deletion_of_files.java
@@ -0,0 +1,19 @@
+package at.campus02.pr3.file;
+
+import java.io.File;
+
+public class ue_rename_and_deletion_of_files {
+
+    public static void main(String[] args) {
+
+        /*
+        Tip: use delete(File file), renameTo(File dest)
+Create a Method
+which creates 10 Files
+save all File Information in a List
+renames all files (print to console original name + name after renaming. Example test.txt -> test_neu.txt + Status
+delete all files and print filename and the status of the deletion.
+
+         */
+    }
+}
