d39e10a22f16c4dfe7b3ab3ab560291dbd7121b1
Your Name <you@example.com>
2021-05-26 10:02:00 +0200

    message

--- src/at/campus02/pr3/fileio/binaryoutput/ue_15.java
+++ src/at/campus02/pr3/fileio/binaryoutput/ue_15.java
@@ -17,18 +17,7 @@ public class ue_15 {
 
      */
     public static void main(String[] args) throws IOException {
-        File file = new File("test-output.txt");
-        FileOutputStream fileOutputStream = new FileOutputStream(file);
 
-        char c;
-
-        while ((c = (char) System.in.read()) != 'x') {
-            fileOutputStream.write(c);
-            System.out.println(c);
-        }
-        fileOutputStream.flush();
-        fileOutputStream.close();
-        System.out.println("Closed");
     }
 
 }
