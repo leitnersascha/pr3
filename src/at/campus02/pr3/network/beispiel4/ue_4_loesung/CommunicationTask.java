ff3731af2d53e0a7ef7764242cc5c669a4a32fd7
Your Name <you@example.com>
2021-05-31 08:59:00 +0200

    message

---
+++ src/at/campus02/pr3/network/beispiel4/ue_4_loesung/CommunicationTask.java
@@ -0,0 +1,10 @@
+package at.campus02.pr3.network.beispiel4.ue_4_loesung;
+
+public class CommunicationTask implements Runnable
+{
+
+	@Override
+	public void run() {
+
+	}
+}
--- src/at/campus02/pr3/network/beispiel4/ue_4_loesung/ListenToMultipleClients.java
+++ src/at/campus02/pr3/network/beispiel4/ue_4_loesung/ListenToMultipleClients.java
@@ -1,4 +1,4 @@
-package at.campus02.pr3.network.beispiel4.lue_4_loesung;
+package at.campus02.pr3.network.beispiel4.ue_4_loesung;
 
 public class ListenToMultipleClients {
     public static void main(String[] args) {
