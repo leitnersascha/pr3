774ae65dc0c9f5221aec392d6456c15bc5679e88
Your Name <you@example.com>
2021-06-01 09:39:11 +0200

    message

--- src/at/campus02/pr3/network/beispiel3/UE_3.java
+++ src/at/campus02/pr3/network/beispiel3/UE_3.java
@@ -25,12 +25,23 @@ public class UE_3 {
 
             // LESEN
             bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            printWriter = new PrintWriter("content.html");
+            PrintWriter printWriter_content = new PrintWriter("content.html");
+            PrintWriter printWriter_response = new PrintWriter("resp.html");
             String line;
             while ((line = bufferedReader.readLine()) != null) {
+                if (line.startsWith("<!DOCTYPE")) // Hier möchte ich nur die Response auslesen und nicht den Content.
+                    break;
+                printWriter_response.println(line);
+                // printWriter_response.flush();
                 System.out.println(line);
-                printWriter.println(line);
             }
+            do {
+                printWriter_content.println(line); // content wird hier reingeschrieben (HTML content)
+                printWriter_content.flush();
+            }
+            while ((line = bufferedReader.readLine()) != null); // bis keine repsonse zurück kommt.
+            printWriter_content.close();
+            printWriter_response.close();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
