def3135b0072d3920c1765e0cd06864a3d48f054
Your Name <you@example.com>
2021-06-01 09:15:40 +0200

    message

--- src/at/campus02/pr3/network/beispiel3/UE_3.java
+++ src/at/campus02/pr3/network/beispiel3/UE_3.java
@@ -1,16 +1,53 @@
 package at.campus02.pr3.network.beispiel3;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class UE_3
-{
-	public static void main(String[] args)
-	{
+public class UE_3 {
+    public static void main(String[] args) {
+        Socket socket = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        PrintWriter printWriter = null;
 
-	}
+        try {
+            socket = new Socket("www.acoela.myspecies.info", 80);
+
+            // SCHREIBEN
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedWriter.write("GET /en HTTP/1.1");
+            bufferedWriter.newLine(); // -> sonst eine Zeile
+            bufferedWriter.write("Host: www.acoela.myspecies.info");
+            bufferedWriter.newLine(); // newline wird 2 mal benötigt!
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            // LESEN
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            printWriter = new PrintWriter("content.html");
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                printWriter.println(line);
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (printWriter != null)
+                    printWriter.close();
+                if (bufferedReader != null)
+                    bufferedReader.close();
+                if (bufferedWriter != null)
+                    bufferedWriter.close();
+                if (socket != null)
+                    socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
