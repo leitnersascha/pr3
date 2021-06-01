4971423237abcb27819d82fbf16e682e4365caa5
Your Name <you@example.com>
2021-06-01 10:16:07 +0200

    message

--- src/at/campus02/pr3/network/beispiel4/ListenToClient.java
+++ src/at/campus02/pr3/network/beispiel4/ListenToClient.java
@@ -15,7 +15,61 @@ public class ListenToClient
 	// start via CommandLine: telnet localhost 9090
 	public static void main(String[] args)
 	{
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		ServerSocket server = null;
+		Socket connectionToClient = null;
 
+		try
+		{
+			System.out.println("Warte...");
+			server = new ServerSocket(9090);
+			connectionToClient = server.accept();
+			br = new BufferedReader(new InputStreamReader(connectionToClient.getInputStream()));
+			bw = new BufferedWriter(new OutputStreamWriter(connectionToClient.getOutputStream()));
+
+			bw.write("HELLO");
+			bw.newLine();
+			bw.flush();
+
+			String command;
+			while ((command = br.readLine()) != null)
+			{
+				if ("TIME".equals(command))
+				{
+					Calendar cal = GregorianCalendar.getInstance();
+					bw.write(cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE) + ":"
+							+ cal.get(Calendar.SECOND));
+					bw.newLine();
+					bw.flush();
+				}
+				else if ("PORT".equals(command))
+				{
+					bw.write("Port:" + connectionToClient.getPort());
+					bw.newLine();
+					bw.flush();
+				}
+				else if ("END".equals(command))
+				{
+					bw.write("Bye");
+					bw.newLine();
+					bw.flush();
+					break;
+				}
+				else
+				{
+					bw.write("unknown command");
+					bw.newLine();
+					bw.flush();
+				}
+			}
+			System.out.println("Beendet");
+
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 
 	}
 
